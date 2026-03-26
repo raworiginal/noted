ad30a84f11b4ebe6c90f8c5e1b7c1ca737a5c04c
Randall Wolfe <randall.a.wolfe@gmail.com>
2026-03-25 19:57:58 -0700

    working on NoteFrom

--- web/components/NoteForm.tsx
+++ web/components/NoteForm.tsx
@@ -1,9 +1,14 @@
-"use client";
 export default function NoteForm() {
   return (
     <form>
       <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-        <legend className="fieldset-legend">Create Note</legend>
+        <legend className="fieldset-legend">Note Form</legend>
+        <label>Title</label>
+        <label>Type</label>
+
+        <label>Body</label>
+        <textarea></textarea>
+        <label></label>
       </fieldset>
     </form>
   );
--- web/types/noteTypes.ts
+++ web/types/noteTypes.ts
@@ -2,13 +2,14 @@ export interface NoteData {
   id: number;
   title: string;
   body: string;
+  type: "text" | "checklist";
   items: ChecklistItem[];
   createdAt: Date;
   updatedAt: Date;
 }
 
 export interface ChecklistItem {
-  id: number;
+  id?: number;
   completed: boolean;
   text: string;
   position: number;
