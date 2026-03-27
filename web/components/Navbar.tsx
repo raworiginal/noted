84ce59f152c51cec3fde0087d12c928893ed9aa6
Randall Wolfe <randall.a.wolfe@gmail.com>
2026-03-27 12:19:29 -0700

    added services

--- web/components/Navbar.tsx
+++ web/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@/lib/auth";
 import LogoutBtn from "./LogoutBtn";
 import { headers } from "next/headers";
+import Link from "next/link";
 export default async function Navbar() {
   const session = await auth.api.getSession({
     headers: await headers(),
@@ -10,7 +11,12 @@ export default async function Navbar() {
     <nav className="navbar bg-accent shadow-md">
       <div className="navbar-start"></div>
       <div className="navbar-center">
-        <p className="color-accent-content text-xl font-bold">NOTED</p>
+        <Link
+          href={"/dashboard"}
+          className="color-accent-content text-xl font-bold"
+        >
+          NOTED
+        </Link>
       </div>
       <div className="navbar-end">{session && <LogoutBtn />}</div>
     </nav>
